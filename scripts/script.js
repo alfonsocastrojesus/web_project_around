b105a7009bf8b2e1013b70a29da27eb014b6460b
castro_alfonso <alfonso.je.castro@gmail.com>
2025-07-29 20:47:22 -0500

    add some js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
-//
+const buttonEdit = document.querySelector(".button_edit");
+const profileCompleteName = document.querySelector(".profile__complete-name");
+const profileOccupation = document.querySelector(".profile__occupation");
+
+const popUp = document.querySelector(".popup");
+const popupButtonClose = popUp.querySelector(".popup__button_close");
+
+const form = popUp.querySelector(".form");
+const nameInput = form.querySelector("#text1");
+const occupationInput = form.querySelector("#text2");
